b6d3588d2cde83fab6731b2fbf973aedc9fd292c
Adith A Danthi <adith.danthi@gmail.com>
2020-04-17 16:20:27 +0530

    final commit

--- Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,11 +2,52 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var diceImage1: ImageView
+    private lateinit var diceImage2: ImageView
+    private lateinit var text: TextView
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollBtn: Button = findViewById(R.id.roll_btn)
+        val resetBtn: Button = findViewById(R.id.reset_btn)
+
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+        text = findViewById(R.id.textView)
+
+        rollBtn.setOnClickListener{ rollDice() }
+
+        resetBtn.setOnClickListener {
+            diceImage1.setImageResource(R.drawable.empty_dice)
+            diceImage2.setImageResource(R.drawable.empty_dice)
+            text.visibility = View.VISIBLE
+        }
+    }
+
+    private fun rollDice() {
+        diceImage1.setImageResource(getRandomDiceImage())
+        diceImage2.setImageResource(getRandomDiceImage())
+        text.visibility = View.GONE
+    }
+
+    private fun getRandomDiceImage(): Int {
+        return when((1..6).random()){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
     }
 }
